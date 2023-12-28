9213c3e260cef5cfcc812440900b5b3f8eee6faf
Felipemelo09 <felipe.melonunes09@gmail.com>
2023-12-27 21:15:47 -0300

    added Motor and MotorRoom classes to manage ship speed

--- core/objects/ship/types/Ship.py
+++ core/objects/ship/types/Ship.py
@@ -3,6 +3,8 @@ from core.general.IDamageable import IDamageable
 from core.objects.attack.PrimaryShot import PrimaryShot
 from core.objects.attack.Shot import Shot
 from core.objects.ship.BaseShipEngine import BaseShipEngine
+from core.objects.ship.room.motor_room.MotorRoom import MotorRoom
+from core.objects.ship.room.motor_room.motor import Motor
 from core.utils.Cooldown import Cooldown
 
 class Ship(BaseShipEngine, IDamageable):
@@ -11,22 +13,46 @@ class Ship(BaseShipEngine, IDamageable):
             self, 
             position: tuple, 
             group: object,
-            sprite: str 
+            sprite: str
         ) -> None:
         
         super().__init__(position, group, sprite)
+
+        # will be outside the class on be definied on a factory method
+        self.motorRoom = MotorRoom(
+            power_required=0.01, motors=[
+                Motor(),
+                Motor(),
+                Motor(),
+                Motor()
+            ]
+        )
+
         self.rooms = [
-            
+            self.motorRoom
         ]
 
-        # not oficial implementation
         self.shoot = Cooldown(200, self.shoot, None)
+        self.motorRoom.set_using_all_motors(1)
 
     def collide(self, collision_object: object):
         if collision_object is not self.last_shot:
             if isinstance(collision_object, PrimaryShot):
                 self.life -= 50
 
+    def get_speed(self) -> float:
+        speed = self.motorRoom.use()
+        print(speed)
+        return speed
+    
+    def acelerate_all(self, aceleration):
+        current_aceleration = self.motorRoom.get_avarage_using()
+        current_aceleration += (aceleration/100)
+        self.motorRoom.set_using_all_motors(current_aceleration)
+
+
+
+    ## not oficical implementation
     def damage(self, damage_object: IDamageObject):
 
         total_damage = damage_object.get_damage()
@@ -35,7 +61,8 @@ class Ship(BaseShipEngine, IDamageable):
         if isinstance(damage_object, Shot):
             self.life -= total_damage
 
-    ## not oficial implementation
+
+    ## not oficial implementation 
     def shoot(self, direction: tuple):
         self.last_shot = PrimaryShot(
             speed=30,
