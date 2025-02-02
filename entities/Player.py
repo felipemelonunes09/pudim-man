8752a0104d3e5d44dae7eeb3b496d00fdbd451ac
felipe-melo <felipe.melonunes09@gmail.com>
2025-02-02 17:53:27 -0300

    feat: added the player class

---
+++ entities/Player.py
@@ -0,0 +1,11 @@
+import pygame
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, *groups):
+        super().__init__(*groups)
+
+    def draw():
+        pass
+
+    def update():
+        pass
