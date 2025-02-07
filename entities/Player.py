b9930780b8935751430fba89f3b953c0e35f1229
felipe-melo <felipe.melonunes09@gmail.com>
2025-02-07 13:35:20 -0300

    feat: added powered features

--- entities/Player.py
+++ entities/Player.py
@@ -5,7 +5,11 @@ from utils.helpers import Direction
 
 class Player(ColiableEntity):
     def __init__(self, *args, **kwargs):
-        self.__isPowered = False
+        self.__isPowered            = False
+        self.__powerDuration        = 10000
+        self.__remainingPowertime   = 0
+        self.__powerStart           = None
+        self.__alive                = True
         super().__init__(
             walkRight   = Sprites.Pudim.walkRight,
             walkLeft    = Sprites.Pudim.walkLeft,
@@ -15,15 +19,31 @@ class Player(ColiableEntity):
             **kwargs
         )
 
-    def getIsPowered(self) -> bool:
+    def isAlive(self) -> bool:
+        return self.__alive
+
+    def IsPowered(self) -> bool:
         return self.__isPowered
+    
+    def getPowerDuration(self) -> int:
+        return self.__powerDuration
+    
+    def getRemainingPowerTime(self) -> int:
+        return self.__remainingPowertime
     
     def setIsPowered(self, powered: bool) -> None:
         self.__isPowered = powered
+        self.__powerStart = pygame.time.get_ticks() if powered else None
 
     def update(self):
         keys = pygame.key.get_pressed()
         self.move(keys)
+        if self.__isPowered:
+            elapsedTime = pygame.time.get_ticks() - self.__powerStart
+            self.__remainingPowertime = max(0, (self.__powerDuration - elapsedTime) // 1000)
+            print(self.__remainingPowertime)
+            if elapsedTime >= self.__powerDuration:
+                self.setIsPowered(False)
         return super().update()
     
     def move(self, keys: pygame.key.ScancodeWrapper):
@@ -35,4 +55,9 @@ class Player(ColiableEntity):
             return super().move(Direction.UP)
         elif keys[pygame.K_DOWN]:
             return super().move(Direction.DOWN)
+        
+    def onCollision(self, entity):
+        if isinstance(entity, Enemy) and not self.IsPowered():
+            self.kill()
+        return super().onCollision(entity)
         
