303b25dad50e2df033a8345db5cf4e8739a437d5
felipe-melo <felipe.melonunes09@gmail.com>
2025-02-07 18:05:06 -0300

    (WIP) for better enemy movs

--- entities/enemy/Enemy.py
+++ entities/enemy/Enemy.py
@@ -8,22 +8,14 @@ class Enemy(ColiableEntity, metaclass=ABCMeta):
 
     def __init__(self, target: Entity, *args, **kwargs):
         self.__target = target
+        self.__currentDirectionVector = Direction.RIGHT
         super().__init__(*args, **kwargs)
 
     def update(self):
-        
-        tPos = self.getTargetPosition()
-
-        if tPos[0] > self.rect.x:
-            self.move(Direction.RIGHT)
-        elif tPos[0] < self.rect.x:
-            self.move(Direction.LEFT)
-        elif tPos[1] > self.rect.y:
-            self.move(Direction.DOWN)
-        else:
-            self.move(Direction.UP)
-
         super().update()
+
+        tPos = self.getTargetPosition()
+        self.move(self.__currentDirectionVector)
 
     def getTargetPosition(self) -> tuple[int, int]:
         return (self.__target.lastX,self.__target.lastY)
@@ -33,3 +25,13 @@ class Enemy(ColiableEntity, metaclass=ABCMeta):
         if isinstance(entity, Player) and entity.IsPowered():
             self.kill()
         return super().onCollision(entity)
+    
+    def changeDirectionVector(self, tPos: tuple[int, int], excludeDirection: Direction = None) -> None:
+        #print(f"should go to the right {tPos[0] > self.rect.x and Direction.RIGHT != excludeDirection}")
+        #print(f"should go to the left {tPos[0] < self.rect.x and Direction.LEFT != excludeDirection}")
+        #print(f"should go to the down {tPos[1] > self.rect.y and Direction.DOWN != excludeDirection}")
+        #print(f"should go to the up {tPos[1] < self.rect.y and Direction.UP != excludeDirection}")
+        #print(f"excluded position {excludeDirection}")
+        #print("=================================")
+        pass
+        
