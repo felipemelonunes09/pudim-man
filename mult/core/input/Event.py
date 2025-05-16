d2c4a97e62b8bc4f090ae99a29265e81c369d3f5
felipe-melo <felipe.melonunes09@gmail.com>
2025-05-15 22:43:42 -0300

    raw

--- mult/core/input/Event.py
+++ mult/core/input/Event.py
@@ -1,9 +1,16 @@
+from typing import Tuple
 from utils.helpers import Direction
 
 class Event():
     pass
 
-class UpdatePlayerPositionEvent(Event):
+class DirectionInputEvent(Event):
     def __init__(self, cardinalDirection: Direction):
         self.cardinalDirection = cardinalDirection
+        super().__init__()
+
+
+class PointInputEvent(Event):
+    def __init__(self, position: Tuple[int, int]):
+        self.position = position
         super().__init__()
