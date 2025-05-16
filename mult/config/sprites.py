943777726edf70d7f390713ff1c2ee8953561f31
felipe-melo <felipe.melonunes09@gmail.com>
2025-05-15 22:42:17 -0300

    refact: refactored sprites classes for handling better scale

--- mult/config/sprites.py
+++ mult/config/sprites.py
@@ -1,15 +1,24 @@
+from typing import Tuple
 import pygame
 from config import globals
 
+class _Sprite:
+    def __init__(self, path: str):
+         self.path = path
+
+    def __call__(self, ratio: Tuple[int, int], size: int = 4) -> list:
+        return [pygame.transform.smoothscale(pygame.image.load(f"{self.path}/{i}.png"), ratio) for i in range(0, size)]
+
 class Sprites:
+    
     class Pudim:
-        walkRight   = [pygame.transform.smoothscale(pygame.image.load(f"{globals.PLAYER_IMAGE_DIR}right/{i}.png"), globals.SPRITE_SCALE_RATIO) for i in range(0, 4)]
-        walkLeft    = [pygame.transform.smoothscale(pygame.image.load(f"{globals.PLAYER_IMAGE_DIR}left/{i}.png"), globals.SPRITE_SCALE_RATIO) for i in range(0, 4)]
-        walkUp      = [pygame.transform.smoothscale(pygame.image.load(f"{globals.PLAYER_IMAGE_DIR}up/{i}.png"), globals.SPRITE_SCALE_RATIO) for i in range(0, 4)]
-        walkDown    = [pygame.transform.smoothscale(pygame.image.load(f"{globals.PLAYER_IMAGE_DIR}down/{i}.png"), globals.SPRITE_SCALE_RATIO) for i in range(0, 4)]
+        walkRight   = _Sprite(f"{globals.PLAYER_IMAGE_DIR}/right")
+        walkLeft    = _Sprite(f"{globals.PLAYER_IMAGE_DIR}/left")
+        walkUp      = _Sprite(f"{globals.PLAYER_IMAGE_DIR}/up")
+        walkDown    = _Sprite(f"{globals.PLAYER_IMAGE_DIR}/down")
     class Pan:
-        walkRight        = [pygame.transform.smoothscale(pygame.image.load(f"{globals.PAN_IMAGE_DIR}/right/{i}.png"), globals.SPRITE_SCALE_RATIO) for i in range(0, 4)]
-        walkLeft         = [pygame.transform.smoothscale(pygame.image.load(f"{globals.PAN_IMAGE_DIR}/left/{i}.png"), globals.SPRITE_SCALE_RATIO) for i in range(0, 4)]
+        walkRight        = _Sprite(f"{globals.PAN_IMAGE_DIR}/right")  
+        walkLeft         = _Sprite(f"{globals.PAN_IMAGE_DIR}/left")
     class Jelly:
-        walkRight        = [pygame.transform.smoothscale(pygame.image.load(f"{globals.JELLY_IMAGE_DIR}/right/{i}.png"), globals.SPRITE_SCALE_RATIO) for i in range(0, 4)]
-        walkLeft         = [pygame.transform.smoothscale(pygame.image.load(f"{globals.JELLY_IMAGE_DIR}/left/{i}.png"), globals.SPRITE_SCALE_RATIO) for i in range(0, 4)]
+        walkRight        = _Sprite(f"{globals.JELLY_IMAGE_DIR}/right")
+        walkLeft         = _Sprite(f"{globals.JELLY_IMAGE_DIR}/left")
